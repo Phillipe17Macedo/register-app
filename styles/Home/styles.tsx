ad4dc2dbc7f34024f08e976a0967df0758ebf487
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-06-12 08:50:16 -0300

    main

---
+++ styles/Home/styles.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
