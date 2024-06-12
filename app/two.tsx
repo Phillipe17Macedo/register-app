c8a1239c8f68b6dd549571ca3ce19e06f6b7a398
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-06-12 17:06:18 -0300

    screen-two-0.0.2

--- app/two.tsx
+++ app/two.tsx
@@ -1,12 +1,86 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import { styles } from '@/styles/Two/styles';
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  Modal,
+} from "react-native";
+import { styles } from "@/styles/Two/styles";
 
 export default function TabTwoScreen() {
+  const [nomeInput, setNomeInput] = useState("");
+  const [telefoneInput, setTelefoneInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+  const [modalVisivel, setModalVisivel] = useState(false);
+
+  const handlerOnPress = () => {
+    console.log("Pressionado !");
+    setModalVisivel(true);
+  };
+
+  const handlerFecharModal = () => {
+    setModalVisivel(false);
+  };
+
+  async function handlerOnLongPress() {
+    Alert.alert("Botão pressionado");
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} />
+      <Text style={styles.title}>Preencha os Dados</Text>
+      <View style={[styles.containertextInput]}>
+        <TextInput
+          placeholder="Nome Completo"
+          style={[styles.textInput]}
+          keyboardType="default"
+          value={nomeInput}
+          onChangeText={setNomeInput}
+        />
+        <TextInput
+          placeholder="Telefone"
+          style={[styles.textInput]}
+          keyboardType="number-pad"
+          value={telefoneInput}
+          onChangeText={setTelefoneInput}
+        />
+        <TextInput
+          placeholder="Email"
+          style={[styles.textInput]}
+          keyboardType="email-address"
+          value={emailInput}
+          onChangeText={setEmailInput}
+        />
+      </View>
+      <TouchableOpacity
+        style={[styles.containerButton]}
+        onPress={handlerOnPress}
+        onLongPress={handlerOnLongPress}
+      >
+        <Text style={[styles.textoButton]}>PRESSIONE !</Text>
+      </TouchableOpacity>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisivel}
+        onRequestClose={handlerFecharModal}
+      >
+        <View style={styles.conatinerModal}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>{nomeInput}</Text>
+            <Text style={styles.modalText}>{telefoneInput}</Text>
+            <Text style={styles.modalText}>{emailInput}</Text>
+            <TouchableOpacity
+              onPress={handlerFecharModal}
+              style={styles.closeButton}
+            >
+              <Text style={styles.closeButtonText}>Fechar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
--- styles/Two/styles.tsx
+++ styles/Two/styles.tsx
@@ -4,16 +4,81 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    flexDirection: 'column',
     backgroundColor: '#fff'
   },
   title: {
-    fontSize: 20,
+    fontSize: 31,
     fontWeight: "bold",
+    fontVariant: ["small-caps"],
+    marginTop: 60,
+    marginBottom: 25,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  containertextInput: {
+    width: '90%',
+    height: 180,
+    borderRadius: 10,
+    //backgroundColor: 'red',
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    marginTop: 10
+  },
+  textInput: {
+    width: '100%',
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    fontWeight: 'bold',
+    color: '#012030',
+    fontSize: 16,
+    backgroundColor: '#fff',
+    elevation: 3,
+  },
+  containerButton: {
+    backgroundColor: '#042940',
+    width: 200,
+    height: 50,
+    borderRadius: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 80,
+    elevation: 5
+  },
+  textoButton: {
+    fontSize: 21,
+    fontWeight: 'bold',
+    fontVariant: ['small-caps'],
+    color: '#DBF227'
+  },
+  conatinerModal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  modalContent: {
+    width: 300,
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 10,
+    fontWeight: 'bold'
+  },
+  closeButton: {
+    backgroundColor: '#DBF227',
+    padding: 12,
+    borderRadius: 21,
+    elevation: 3,
+    marginTop: 10,
+  },
+  closeButtonText: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 18
   },
 });
